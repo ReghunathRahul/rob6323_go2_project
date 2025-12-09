dac8095367cd7a74811e74408bd068c5d75ce453
Long Quang <longpquang@gmail.com>
2025-12-09 11:11:22 -0500

    task refactor

--- source/rob6323_go2/rob6323_go2/tasks/robust_locomotion/rob6323_go2/rob6323_go2_uneven_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/robust_locomotion/rob6323_go2/rob6323_go2_uneven_env_cfg.py
@@ -7,7 +7,7 @@ from isaaclab import sim as sim_utils
 from isaaclab.terrains import TerrainImporterCfg
 from isaaclab.markers.config import BLUE_ARROW_X_MARKER_CFG
 
-from .rob6323_go2_env_cfg import Rob6323Go2EnvCfg
+from rob6323_go2.tasks.direct.rob6323_go2.rob6323_go2_env_cfg import Rob6323Go2EnvCfg
 
 @configclass
 class Rob6323Go2UnevenEnvCfg(Rob6323Go2EnvCfg):
