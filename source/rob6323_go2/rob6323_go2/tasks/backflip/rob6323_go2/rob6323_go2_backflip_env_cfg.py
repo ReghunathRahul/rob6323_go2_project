694864ec11d8ec13a835b282eccc7d3e1585a670
ReghunathRahul <reghunathrahul2000@gmail.com>
2025-12-18 19:36:48 -0500

    Added init to config to provide

--- source/rob6323_go2/rob6323_go2/tasks/backflip/rob6323_go2/rob6323_go2_backflip_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/backflip/rob6323_go2/rob6323_go2_backflip_env_cfg.py
@@ -1,33 +1,25 @@
-"""
-This configuration adapts the horizon, observation space, and reward weights for a backflip cycle.
-"""
 
 from isaaclab.utils import configclass
-
 from rob6323_go2.tasks.direct.rob6323_go2.rob6323_go2_env_cfg import Rob6323Go2EnvCfg
-
+import torch
 
 @configclass
 class Rob6323Go2BackflipEnvCfg(Rob6323Go2EnvCfg):
     """
-    Backflip-specific tuning
+    Backflip specific tuning 
     """
 
-    # shorter episodes for a clean flip and landing rather than locomotion.
     episode_length_s = 1.2
-    
+    flip_period_s = 0.9
 
-    # add features (sin/cos) to help the policy track where it is in flip.
-    observation_space = Rob6323Go2EnvCfg().observation_space + 2
-
-    action_scale = 2.5
+    action_scale = 1.0
+    action_smoothness_scale = 0.05 
     debug_vis = False
 
-    # curriculum learning settings
     curriculum_duration_steps: float = 5_000_000.0
 
-    # task timing and reward shaping parameters
-    flip_period_s: float = 0.9
+    observation_space = Rob6323Go2EnvCfg().observation_space + 2
+
     takeoff_phase_portion: float = 0.25
     airborne_phase_start: float = 0.25
     airborne_phase_end: float = 0.75
@@ -35,13 +27,30 @@ class Rob6323Go2BackflipEnvCfg(Rob6323Go2EnvCfg):
     air_contact_force_limit: float = 120.0
     landing_pitch_sigma: float = 0.25
     landing_force_sigma: float = 120.0
-    action_smoothness_scale: float = 0.0
     orientation_reward_scale: float = 1.5
     takeoff_vel_reward_scale: float = 0.6
     airborne_reward_scale: float = 0.4
     landing_reward_scale: float = 0.7
     target_flip_speed: float = -8.0
 
-    # environment setup
     scene = Rob6323Go2EnvCfg().scene.replace(num_envs=2048, env_spacing=5.0)
+    scene.terrain.static_friction = 0.7
+    scene.terrain.dynamic_friction = 0.6
     contact_sensor = Rob6323Go2EnvCfg().contact_sensor.replace(history_length=5)
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        if self.actuators is not None:
+             for key in self.actuators.keys():
+                self.actuators[key].stiffness = 60.0
+                self.actuators[key].damping = 5.0 
+
+        self.initial_state.pos = (0.0, 0.0, 0.55)
+        self.initial_state.rot = (1.0, 0.0, 0.0, 0.0)
+        self.initial_state.joint_pos = {
+            ".*hip_joint": 0.0,
+            ".*thigh_joint": 0.8,
+            ".*calf_joint": -1.5,
+        }
+        self.initial_state.joint_vel = {".*": 0.0}
